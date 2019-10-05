cc15229dca9a4a7aa2244894834dd97bd98f1f3c
Joseph Hentges <hentgesj@augsburg.edu>
2019-10-05 15:59:14 -0500

    second

---
+++ main.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        Boolean[] tfArray = new Boolean[number];
+        String[] characters = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < number; i += 1) {
+            if (characters[i].equals("T")) {
+                tfArray[i] = true;
+            } else if (characters[i].equals("F")) {
+                tfArray[i] = false;
+            }
+        }
+
+        ArrayList<Boolean> stack = new ArrayList<>();
+        int counter = 0;
+
+        String tempLine = scanner.nextLine();
+
+        // System.out.println("Temp Line = " + tempLine);
+
+        String lineScan[] = tempLine.split(" ");
+
+        // System.out.println("Before While Loop");
+
+        for (int i = 0; i < lineScan.length; i++) {
+            // System.out.println("Inside While");
+            String temp = lineScan[i];
+            // System.out.println(":" + temp);
+            if (temp.equals("+")) {
+                // System.out.println("Plus");
+                Boolean first = stack.remove(stack.size() - 1);
+                Boolean second = stack.remove(stack.size() - 1);
+                stack.add(first || second);
+            } else if (temp.equals("*")) {
+                // System.out.println("Mult");
+                Boolean first = stack.remove(stack.size() - 1);
+                Boolean second = stack.remove(stack.size() - 1);
+                stack.add(first && second);
+            } else if (temp.equals("-")) {
+                // System.out.println("Sub");
+                Boolean first = stack.remove(stack.size() - 1);
+                stack.add(!first);
+            } else {
+                // System.out.println("Add To Stack");
+                stack.add(tfArray[counter]);
+                counter += 1;
+            }
+        }
+
+        if(stack.get(stack.size()-1)) {
+            System.out.println("T");
+        } else {
+            System.out.println("F");
+        }
+
+        System.exit(0);
+    }
+}
---
+++ research_productivity_index/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] temps = scanner.nextLine().split(" ");
+
+        int[][] output = new int[Integer.parseInt(temps[1])][2];
+        
+        for (int i = 0; i < Integer.parseInt(temps[0]); i += 1) {
+            String[] line = scanner.nextLine().split(" ");
+
+            output[Integer.parseInt(line[0])-1][0] += Integer.parseInt(line[1]);
+            output[Integer.parseInt(line[0])-1][1] += Integer.parseInt(line[2]);
+        }
+
+        String[] lineOutputs = new String[output.length+1];
+
+        for (int i = 0; i < output.length; i += 1) {
+            int aPer = output[i][0];
+            int bPer = output[i][1];
+            int temp = ((aPer + bPer) / 2) + 1;
+            System.out.println(temp);
+            String winner = "";
+            int aWaste = 0;
+            int bWaste = 0;
+            if (aPer > bPer) {
+                winner = "A";
+                aWaste = aPer - temp;
+                bWaste = bPer;
+            } else {
+                winner = "B";
+                aWaste = aPer;
+                bWaste = bPer - temp;
+            }
+            System.out.println(winner + " " + aPer + " " + bPer);
+            lineOutputs[i] = winner + " " + aPer + " " + bPer;
+        }
+        System.out.println("\n");
+        for (int i = 0; i < lineOutputs.length; i += 1) {
+            System.out.println(lineOutputs[i]);
+        }
+    }
+}
